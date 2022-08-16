630f6b6de64f90d91acc7a031457da107f43b086
FinBy <fin_by@mail.ru>
2022-08-16 15:20:16 +0300

    from IDE

---
+++ Application.java
@@ -0,0 +1,10 @@
+public class Application {
+    public static void main(String[] args) {
+//        System.out.println("Hello World !!! ");
+        int a = 5;
+        int b = 2;
+
+        int result = a + b;
+        System.out.println(result);
+    }
+}
